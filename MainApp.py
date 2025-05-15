24694d458145fc88afeaddf8f06aec88db6b4a9c
Josef Borik <joseph.borik@gmail.com>
2025-05-15 12:06:10 +0200

    VÚSC -> VÚP, změna vykreslování vrstev (#14)
    * Úprava kódu kvůli zpřesnění pojmu, Změna pořad vykreslování a názvů vrstev
    
    viz https://cuzk.gov.cz/ruian/Poskytovani-udaju-ISUI-RUIAN-VDP/Ciselniky-ISUI/Vyssi-uzemni-prvky-a-uzemne-evidencni-jednotky.aspx a https://cuzk.gov.cz/ruian/Poskytovani-udaju-ISUI-RUIAN-VDP/Ciselniky-ISUI/Nizsi-uzemni-prvky-a-uzemne-evidencni-jednotky.aspx

--- MainApp.py
+++ MainApp.py
@@ -340,9 +340,9 @@ class MainApp(QDialog):
         """
         self.option['layers'] = []
         self.option['layers_name'] = []
-        #pridani vusc pokud zaskrtnuto
-        if self.ui.vuscCheckbox.isChecked():
-            self.option['layers'].append('VUSC')
+        #pridani VUP pokud zaskrtnuto
+        if self.ui.vupCheckbox.isChecked():
+            self.option['layers'].append('VUP')
 
         for row in range(0,self.model.rowCount()):
             item = self.model.item(row,0)
@@ -360,7 +360,7 @@ class MainApp(QDialog):
         }
         # not supported yet
         selectionIndex = self.ui.selectionComboBox.currentIndex()
-        if selectionIndex == 1 and 'VUSC' in self.option['layers']:
+        if selectionIndex == 1 and 'VUP' in self.option['layers']:
             vfr_type['zgho'] = 'G'
         # elif selectionIndex == 2:
         #     vfr_type['zgho'] = 'Z'
@@ -494,23 +494,23 @@ class MainApp(QDialog):
         layerGroup = root.addGroup(groupName)
 
         # first add well-known layers
-        for layer_name, layer_alias in [('obce', 'Obce'),
-                                        ('spravniobvody', 'Správní obvody'),
-                                        ('mop', 'Městské obvody v Praze'),
-                                        ('momc', 'Městský obvod/část'),
+        for layer_name, layer_alias in [('adresnimista', 'Adresní místa'),
+                                        ('stavebniobjekty', 'Stavební objekty'),
+                                        ('parcely', 'Parcely'),
+                                        ('ulice', 'Ulice'),
+                                        ('zsj', 'Základní sídelní jednotky'),
                                         ('castiobci', 'Části obcí'),
                                         ('katastralniuzemi', 'Katastrální území'),
-                                        ('zsj', 'Základní sídelní jednotky'),
-                                        ('ulice', 'Ulice'),
-                                        ('parcely', 'Parcely'),
-                                        ('stavebniobjekty', 'Stavební objekty'),
-                                        ('adresnimista', 'Adresní místa'),
-                                        ('staty', 'Stát'),
-                                        ('regionysoudrznosti', 'Regiony soudrznosti'),
-                                        ('vusc', 'VUSC'),
-                                        ('okresy', 'Okresy'),
+                                        ('momc', 'Městské obvody/části'),
+                                        ('spravniobvody', 'Správní obvody v Praze'),
+                                        ('mop', 'Obvody v Praze'),
+                                        ('obce', 'Obec'),
+                                        ('po', 'Obce s POÚ'),
                                         ('orp', 'ORP'),
-                                        ('po', 'POU')]:
+                                        ('okresy', 'Okresy'),
+                                        ('vusc', 'VÚSC (nové kraje)'),
+                                        ('regionysoudrznosti', 'Regiony soudržnosti'),
+                                        ('staty', 'Stát')]:
             layer = datasource.GetLayerByName(layer_name)
             if layer:
                 if not add_layer(layerGroup, layer, layer_alias):
@@ -559,7 +559,7 @@ class ImportThread(QThread):
             n = len(self.layers)         
             i = 1
             for l in self.layers:
-                if l == 'VUSC':
+                if l == 'VUP':
                     filename = 'ST_{}'.format(self.file_type)
                 else:
                     filename = 'OB_{}_{}'.format(l, self.file_type)
