14a7aa26cc74cd00ac5b02ee25cadb277597b035
hadlimar <marek.hadlik@seznam.cz>
2025-06-23 13:54:04 +0200

    Add files via upload

--- MainApp.py
+++ MainApp.py
@@ -49,49 +49,7 @@ class TextOutputSignal(QObject):
 
 class MainApp(QDialog):
 
-     def add_layers(self):
-        def add_layer(group, ogr_layer, layer_alias):
-            if ogr_layer.GetFeatureCount() < 1:
-                return False
-
-            layer_name = ogr_layer.GetName()
-            vlayer = QgsVectorLayer('{0}|layername={1}'.format(self.option['datasource'], layer_name),
-                                    layer_alias, 'ogr')
-
-            vlayer.setCrs(QgsCoordinateReferenceSystem("EPSG:5514"))
-            vlayer.setProviderEncoding('UTF-8')
-
-            layer_style = os.path.join(style_path, layer_name.lower() + '.qml')
-            if os.path.exists(layer_style):
-                vlayer.loadNamedStyle(layer_style)
-
-            QgsProject.instance().addMapLayer(vlayer, addToLegend=False)
-            group.addLayer(vlayer)
-
-            if layer_name == 'parcely':
-                ciselnik = {}
-                csv_path = os.path.join(os.path.dirname(__file__), 'files', 'SC_D_POZEMKU.csv')
-                try:
-                    with open(csv_path, encoding='utf-8') as f:
-                        reader = csv.DictReader(f, delimiter=';')
-                        for row in reader:
-                            ciselnik[row['KOD']] = row['NAZEV']
-                except Exception as e:
-                    QgsMessageLog.logMessage(f"Chyba při čtení číselníku: {e}", level=Qgis.Critical)
-
-                if 'DruhPozemku' not in vlayer.fields().names():
-                    vlayer.dataProvider().addAttributes([QgsField('DruhPozemku', QVariant.String)])
-                    vlayer.updateFields()
-
-                with edit(vlayer):
-                    for feature in vlayer.getFeatures():
-                        kod = str(feature['DruhPozemkuKod'])
-                        nazev = ciselnik.get(kod, 'Neznámý')
-                        feature['DruhPozemku'] = nazev
-                        vlayer.updateFeature(feature)
-
-            return True
-
+    
     def __init__(self, iface, parent=None):
         QDialog.__init__(self)
 
@@ -515,6 +473,28 @@ class MainApp(QDialog):
 
             QgsProject.instance().addMapLayer(vlayer, addToLegend=False)
             group.addLayer(vlayer)
+
+            if layer_name == 'parcely':
+                 ciselnik = {}
+                 csv_path = os.path.join(os.path.dirname(__file__), 'files', 'SC_D_POZEMKU.csv')
+                 try:
+                    with open(csv_path, encoding='cp1250') as f:
+                        reader = csv.DictReader(f, delimiter=';')
+                        for column in reader:
+                            ciselnik[int(column['KOD'])] = column['NAZEV']
+                 except Exception as e:
+                    QgsMessageLog.logMessage(f"Chyba při čtení číselníku: {e}", level=Qgis.Critical)
+
+                 if 'DruhPozemku' not in vlayer.fields().names():
+                    vlayer.dataProvider().addAttributes([QgsField('DruhPozemku', QVariant.String)])
+                    vlayer.updateFields()
+
+                 with edit(vlayer):
+                    for feature in vlayer.getFeatures():
+                        kod = (feature['DruhPozemkuKod'])
+                        nazev = ciselnik.get(kod, 'Neznámý')
+                        feature['DruhPozemku'] = nazev
+                        vlayer.updateFeature(feature)
 
             return True
 
